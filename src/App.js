42e0d6c5b369fdb15bee349e0d3ed8916ad5c469
AkshatChauhan <akshatchauhan1346@gmail.com>
2025-04-11 15:07:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import "./App.css";
+import React from "react";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Menu from "./pages/Menu";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import ResponsiveComponent from "./components/ResponsiveComponent";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Navbar />
+        <ResponsiveComponent />
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/menu" exact component={Menu} />
