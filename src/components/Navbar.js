fddc57666b872b84cbed3d11914c663f7d79d95d
AkshatChauhan <akshatchauhan1346@gmail.com>
2025-04-11 15:20:23 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,23 +10,21 @@ function Navbar() {
   const toggleNavbar = () => {
     setOpenLinks(!openLinks);
   };
+
   return (
     <div className="navbar">
-      <div className="leftSide" id={openLinks ? "open" : "close"}>
-        <img src={Logo} />
-        <div className="hiddenLinks">
+      <div className="leftSide">
+        <img src={Logo} alt="Pizza Logo" />
+      </div>
+
+      <div className="rightSide">
+        <div className={`links ${openLinks ? "show" : ""}`}>
           <Link to="/"> Home </Link>
           <Link to="/menu"> Menu </Link>
           <Link to="/about"> About </Link>
           <Link to="/contact"> Contact </Link>
         </div>
-      </div>
-      <div className="rightSide">
-        <Link to="/"> Home </Link>
-        <Link to="/menu"> Menu </Link>
-        <Link to="/about"> About </Link>
-        <Link to="/contact"> Contact </Link>
-        <button onClick={toggleNavbar}>
+        <button onClick={toggleNavbar} className="toggleButton">
           <ReorderIcon />
         </button>
       </div>
